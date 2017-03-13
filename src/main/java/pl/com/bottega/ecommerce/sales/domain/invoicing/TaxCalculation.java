b4b0555daf0001bed4e874754fee110e6170b237
Gos.Konrad <194948@edu.p.lodz.pl>
2017-03-13 02:47:10 +0100

    added TaxCalculation class

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/TaxCalculation.java
@@ -0,0 +1,40 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+import java.math.BigDecimal;
+
+/**
+ * Created by Konrad Gos on 13.03.2017.
+ */
+public class TaxCalculation {
+    public Tax calculate(ProductType productType, Money net){
+        BigDecimal ratio = null;
+        String desc = null;
+        RequestItem item = new RequestItem(null, 0, net);
+
+        switch (item.getProductData().getType()) {
+            case DRUG:
+                ratio = BigDecimal.valueOf(0.05);
+                desc = "5% (D)";
+                break;
+            case FOOD:
+                ratio = BigDecimal.valueOf(0.07);
+                desc = "7% (F)";
+                break;
+            case STANDARD:
+                ratio = BigDecimal.valueOf(0.23);
+                desc = "23%";
+                break;
+
+            default:
+                throw new IllegalArgumentException(item.getProductData().getType() + " not handled");
+        }
+
+        Money taxValue = net.multiplyBy(ratio);
+
+        Tax tax = new Tax(taxValue, desc);
+
+        return tax;
+    }
+}
