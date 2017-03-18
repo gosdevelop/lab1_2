a80d8e768d8e1717ae25d77d035e7809998bad2c
Gos.Konrad <194948@edu.p.lodz.pl>
2017-03-18 18:06:10 +0100

    optimised and refactor BookKeeper class

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
@@ -15,17 +15,12 @@
  */
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
-import java.math.BigDecimal;
 import java.util.List;
-
-import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
-import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class BookKeeper {
 
 	private InvoiceFactory invoiceFactory;
-
 	private TaxCalculationInterface taxCalculationInterface = new TaxCalculation();
 
 	public Invoice issuance(InvoiceRequest invoiceRequest, List<RequestItem> items) {
